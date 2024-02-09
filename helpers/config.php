d029d38e410033a2a98ce25413fc7a647daef797
22jn0102 <156864389+22jn0102@users.noreply.github.com>
2024-02-09 15:52:25 +0900

    接続を732→azureへ

--- helpers/config.php
+++ helpers/config.php
@@ -1,5 +1,6 @@
 <?php
     // DB接続設定
-    define('DSN', 'sqlsrv:server=10.32.97.1;database=20jn0300');
-    define('DB_USER', '20jn0300');
-    define('DB_PASSWORD', '20jn0300');
+    define('DSN', "sqlsrv:server = tcp:22jn0102db.database.windows.net,1433; Database = 22jn0102DB");
+    define('DB_USER', 'jndb');
+    define('DB_PASSWORD', 'Pa$$word1234');
+    
